75bbfe954a4df8fd588b4174576650856f438efc
Leo Joy <36624136+secretleojoy@users.noreply.github.com>
2021-01-19 10:27:26 +0530

    Create simple multiplication.py

---
+++ simple multiplication.py
@@ -0,0 +1,8 @@
+#python code for simple multiplication
+#altering the codes doesn't make you a programmer
+#coded by Leo Joy
+print("Hello guys lets see the result of 1456*3987")
+a=1456;
+b=3987;
+print("The product of the above numbers is", a*b)
+print("THANK YOU")
